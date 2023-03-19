059711833f07b1134230509342bef6ed00817105
AL-Kahfi04 <muhhamadkahfi185@gmail.com>
2023-03-19 11:10:10 +0700

    2

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,3 +1,6 @@
-<div class="container">
-    <h1>selamat</h1>
+<div class="w-100 " style="background:url('https://i.ytimg.com/vi/WxxF-n4kZqE/maxresdefault.jpg')  ; background-repeat:no-repeat;      height: 100vh;   background-size: cover;
+    ">
+    <div class="container" >
+        <h1>selamat</h1>
+    </div>
 </div>
